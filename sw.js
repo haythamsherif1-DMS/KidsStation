619a9d3d8b3165e3935ff0a9dff4b9382971814d
haythamsherif1-DMS <haythamsherif1@gmail.com>
2026-09-15 11:06:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'edrak-cache-v1.42';
+const CACHE_NAME = 'edrak-cache-v1.43';
 // قائمة الملفات والصور اللي عاوزينها تتحمل وتتحفظ
-const CACHE_NAME = 'edrak-cache-v1.42'; // ارفع الإصدار هنا أيضاً
+const CACHE_NAME = 'edrak-cache-v1.43'; // ارفع الإصدار هنا أيضاً
 const urlsToCache = [
-  './index.html?v=1.42',
-  './manifest.json?v=1.42',
-  './script.js?v=1.42',
-  './icon.png?v=1.42', // أضف الأيقونة هنا برقم إصدار جديد
+  './index.html?v=1.43',
+  './manifest.json?v=1.43',
+  './script.js?v=1.43',
+  './icon.png?v=1.43', // أضف الأيقونة هنا برقم إصدار جديد
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
 ];
